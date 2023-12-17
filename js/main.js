e98e3bef43fca214697ade88902b87f96b33cfe7
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-12-18 00:28:11 +0530

    remove bg color and add animated

--- js/main.js
+++ js/main.js
@@ -5,31 +5,22 @@ c.height = 500;
 document.body.appendChild(c);
 
 var perm = [];
-let val;
 while (perm.length < 255) {
-  while (perm.includes((val = Math.floor(Math.random() * 255)))) ;
+  while (perm.includes((val = Math.floor(Math.random() * 255))));
   perm.push(val);
 }
-var lerp = (a, b, t) => a + (b - a) * (1 - Math.cos(t * Math.PI))/2;
-var noise = (x) => {
+var lerp = (a, b, t) => a + (b - a) * (1 - Math.cos(t*Math.PI))/2;
+var noise = x => {
   // width size
   x = x * 0.01 % 255
   return lerp(perm[Math.floor(x)], perm[Math.ceil(x)], x - Math.floor(x));
 };
 
-/* Background Color */
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
 /* loop background */
+var t = 0;
 function loop() {
-  ctx.fillStyle = getRandomColor();
+  t += 1;
+  ctx.fillStyle = "#19f";
   ctx.fillRect(0, 0, c.width, c.height);
 
   ctx.fillStyle = "black";
@@ -39,14 +30,12 @@ function loop() {
 
   // adjust height
   for (let i = 0; i < c.width; i++)
-    ctx.lineTo(i, c.height - noise(i) * 0.5);
+    ctx.lineTo(i, c.height - noise(t + i) * 0.3);
   ctx.lineTo(c.width, c.height);
   ctx.fill();
+
+  requestAnimationFrame(loop);
+
 }
 
 loop();
-
-// set the change interval
-setInterval(function () {
-  requestAnimationFrame(loop);
-}, 50000);
